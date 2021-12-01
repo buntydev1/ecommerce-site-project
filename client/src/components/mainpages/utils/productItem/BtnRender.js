ab336bbbcf79e24a4bbc6c4dc10ba66bd7c4d631
yasin <buntyvirani82@gmail.com>
2021-12-01 04:37:49 -0800

    admin page updated

--- client/src/components/mainpages/utils/productItem/BtnRender.js
+++ client/src/components/mainpages/utils/productItem/BtnRender.js
@@ -3,25 +3,14 @@ import { Link } from "react-router-dom";
 import { GlobalState } from "../../../../GlobalState";
 
 function BtnRender({ product }) {
-  //   const state = useContext(GlobalState);
-  //   const [isAdmin] = state.userAPI.isAdmin;
-  //   const addCart = state.userAPI.addCart;
+  const state = useContext(GlobalState);
+  const [isAdmin] = state.userAPI.isAdmin;
 
   return (
     <div className="row_btn">
-      <Link id="btn_buy" to="#!">
-        Buy
-      </Link>
-      <Link id="btn_view" to={`detail/${product._id}`}>
-        View
-      </Link>
-      {/* {isAdmin ? (
+      {isAdmin ? (
         <>
-          <Link
-            id="btn_buy"
-            to="#!"
-            onClick={() => deleteProduct(product._id, product.images.public_id)}
-          >
+          <Link id="btn_buy" to="#!">
             Delete
           </Link>
           <Link id="btn_view" to={`/edit_product/${product._id}`}>
@@ -30,14 +19,14 @@ function BtnRender({ product }) {
         </>
       ) : (
         <>
-          <Link id="btn_buy" to="#!" onClick={() => addCart(product)}>
+          <Link id="btn_buy" to="#!">
             Buy
           </Link>
           <Link id="btn_view" to={`/detail/${product._id}`}>
             View
           </Link>
         </>
-      )} */}
+      )}
     </div>
   );
 }
--- client/src/components/mainpages/utils/productItem/ProductItem.js
+++ client/src/components/mainpages/utils/productItem/ProductItem.js
@@ -3,9 +3,10 @@ import React from "react";
 // import { Link } from "react-router-dom";
 import BtnRender from "./BtnRender";
 
-function ProductItem({ product }) {
+function ProductItem({ product, isAdmin }) {
   return (
     <div className="product_card">
+      {isAdmin && <input type="checkbox" checked={product.checked} />}
       <img src={product.images.ul} alt="" />
       <div className="product_box">
         <h2 title={product.title}>{product.title}</h2>
