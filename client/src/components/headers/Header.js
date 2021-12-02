a1d6c205e68e980ac52b43f216cc2782ccd38f8d
yasin <buntyvirani82@gmail.com>
2021-12-02 09:55:49 -0800

    header updated

--- client/src/components/headers/Header.js
+++ client/src/components/headers/Header.js
@@ -15,7 +15,7 @@ function Header() {
 
   const logoutUser = async () => {
     await axios.get("/user/logout");
-
+    localStorage.removeItem("firstLogin");
     window.location.href = "/";
   };
 
