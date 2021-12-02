1549fd1b4fe3a65409296f99d89aa3fb44e22d9a
yasin <buntyvirani82@gmail.com>
2021-12-02 10:44:31 -0800

    product updated

--- client/src/api/ProductsAPI.js
+++ client/src/api/ProductsAPI.js
@@ -1,17 +1,9 @@
-import { React, useState, useEffect } from "react";
+import { useState } from "react";
 // import { GlobalState } from "../../../GlobalState";
-import axios from "axios";
 
 function ProductsAPI() {
   const [products, setProducts] = useState([]);
-  const getProducts = async () => {
-    const res = await axios.get("/api/products");
-    setProducts(res.data.products);
-    // setResult(res.data.result)
-  };
-  useEffect(() => {
-    getProducts();
-  }, []);
+
   return {
     products: [products, setProducts],
   };
